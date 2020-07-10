1d87acfe03181da1fc226191e9ebe5b975d649ea
lauren <ljdutton2@icloud.com>
2020-07-09 17:50:32 -0700

    heroku

--- data/greendb.js
+++ data/greendb.js
@@ -1,3 +1,6 @@
+const mongoose = require('mongoose');
+
+
 //Database
 mongoose.connect(process.env.MONGODB_URI||'mongodb://127.0.0.1:27017/greendb', {useNewUrlParser: true })
 .then(() => console.log("Connected to database"))
