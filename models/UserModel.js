3eac94fdc2912e3d596b22f23e2b0dae55a70042
lauren <ljdutton2@icloud.com>
2020-07-09 21:16:35 -0700

    fix userSchema

--- models/UserModel.js
+++ models/UserModel.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema
 
 const UserSchema = new Schema({
-username: String,
-years: Number,
-branch: String,
-posts: [{ type: Schema.Types.ObjectId, ref: "post" }],
+username: {type: String, unique: true, required: true},
+years: {type: Number},
+branch: {type: String}
+
 
 })
 
