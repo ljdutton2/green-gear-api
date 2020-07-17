2d3abe382ca7c3f07c6fb4d86b98e793be1659e8
lauren <ljdutton2@icloud.com>
2020-07-17 16:45:10 -0700

    jwt

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,13 +1,17 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema
 
+
 const UserSchema = new Schema({
 username: {type: String, unique: true, required: true},
 years: {type: String},
 branch: {type: String}
 
-
 })
+
+
+
+  
 
 
 module.exports = mongoose.model('user', UserSchema)
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express()
+const connectEnsureLogin = require('connect-ensure-login');
+const passportLocalMongoose = require('passport-local-mongoose');
+
 require('dotenv').config()
 
 
@@ -17,6 +20,8 @@ app.use(express.json())
 //Controllers
 const PostControl = require('./controllers/PostControl')
 const UserControl = require('./controllers/UserControl')
+const AuthControl = require('./controllers/AuthControl')
+
 //Routes Posts
 app.post('/api/post/create', PostControl.create)
 app.post('/api/post/update', PostControl.update)
@@ -30,31 +35,39 @@ app.get('/api/user/retrieveOne/:userId', UserControl.retrieveOne)
 app.get('/api/user/retrieveAll', UserControl.retrieveAll)
 app.delete('/api/user/delete', UserControl.delete)
 
-/*  PASSPORT SETUP  */
-
-const passport = require('passport');
-const UserModel = require('../models/UserModel');
-app.use(passport.initialize());
-app.use(passport.session());
-const UserSchema = UserModel.UserSchema;
-
-UserSchema.plugin(passportLocalMongoose);
-/* PASSPORT LOCAL AUTHENTICATION */
-
-passport.use(UserDetails.createStrategy());
-
-passport.serializeUser(UserSchema.serializeUser());
-passport.deserializeUser(UserSchema.deserializeUser());
-
-
-
-
-
+//Routes auth
+app.post('/login',AuthControl.login)
+app.get('/login',AuthControl.login)
+//add random locked route to test
+app.get('/private',
+  connectEnsureLogin.ensureLoggedIn(),
+  (req, res) => res.sendFile('/private.html', {root: __dirname})
+);
 
   // INDEX
     app.get('/', (req, res) => {
+      
        res.send("Welcome to the green gear API, please navigate to /api/post/create")
         })
+
+//JWT
+var cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+app.use(cookieParser()); 
+
+var checkAuth = (req, res, next) => {
+  console.log("Checking authentication");
+  if (typeof req.cookies.nToken === "undefined" || req.cookies.nToken === null) {
+    req.user = null;
+  } else {
+    var token = req.cookies.nToken;
+    var decodedToken = jwt.decode(token, { complete: true }) || {};
+    req.user = decodedToken.payload;
+  }
+
+  next();
+};
+app.use(checkAuth);
     
 
 //Start Server
